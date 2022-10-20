75d04c6d9e7760cd86bd5fbbda9cc03816c07b22
Anna <baffy.009@yandex.ru>
2022-10-21 00:20:22 +0300

    first commit

---
+++ Ex64/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 64: Задайте значение N и M. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Выполнить с помощью рекурсии.
+
+int InputIntNumberM()
+{
+    while (true)
+    try
+        {
+            Console.Write("Введите значение M: ");
+            int M = int.Parse(Console.ReadLine() ?? "0");
+            return M;
+        }
+        catch 
+        {
+            Console.WriteLine("Введите натуральное число");
+        }
+}
+
+int InputIntNumberN()
+{
+    while (true)
+    try
+        {
+            Console.Write("Введите значение N: ");
+            int N = int.Parse(Console.ReadLine() ?? "0");
+            return N;
+        }
+        catch 
+        {
+            Console.WriteLine("Введите натуральное число");
+        }
+}
+
+int M = InputIntNumberM();
+int N = InputIntNumberN();
+
+string NumbersRecUp(int M, int N) 
+{  
+    if (M % 2 == 0)
+    {
+        if (M < N) return $"{M} " + NumbersRecUp(M + 2, N);
+        else return String.Empty;
+    }
+    else
+    {
+        if (M < N) return $"{M + 1} " + NumbersRecUp(M + 2, N);
+        else return String.Empty;   
+    }
+}
+
+string NumbersRecDown(int M, int N) 
+{
+    if (M % 2 == 0)
+    {
+        if (N < M) return $"{M} " + NumbersRecDown(M - 2, N);
+        else return String.Empty;
+    }
+    else
+    {
+        if (N <= M) return $"{M - 1} " + NumbersRecDown(M - 2, N);
+        else return String.Empty;   
+    }
+}
+
+
+Console.WriteLine($"M = {M}, N = {N}");
+Console.WriteLine();
+
+if (N >= M)
+{
+    Console.WriteLine(NumbersRecUp(M, N));
+}
+else
+{
+    Console.WriteLine(NumbersRecDown(M, N));
+}
---
+++ Ex68/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+
+int InputIntNumberM()
+{
+    while (true)
+    try
+        {
+            Console.Write("Введите число M: ");
+            int M = int.Parse(Console.ReadLine() ?? "0");
+            return M;
+        }
+        catch 
+        {
+            Console.WriteLine("Введите целое число, пожалуйста");
+        }
+}
+
+int InputIntNumberN()
+{
+    while (true)
+    try
+        {
+            Console.Write("Введите число N: ");
+            int N = int.Parse(Console.ReadLine() ?? "0");
+            return N;
+        }
+        catch 
+        {
+            Console.WriteLine("Введите целое число, пожалуйста");
+        }
+}
+
+int M = InputIntNumberM();
+int N = InputIntNumberN();
+
+void Recursion(int M, int N)
+{
+    if (M > N)
+    {
+        M = M % N;
+    }
+    else
+    {
+        N = N % M;
+    }
+    if (M != 0 && N != 0)
+    {
+        Recursion(M, N);
+    }
+    else
+    {
+        Console.WriteLine("НОД "+ (M+N).ToString());
+    }
+}
+
+Recursion(M, N);
